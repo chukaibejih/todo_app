dc98138e18a19c33f0bc7dfb4e6b5765232e4a58
Ibejih <chukaibejih@gmail.com>
2022-10-05 08:28:13 +0100

    Hid secret key and worked on base css

--- TODO_APP/settings.py
+++ TODO_APP/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dg2genha)5yyv0s2s=_()#b5b2*gt+8wnuv7)qw=_l__94q6%^'
+SECRET_KEY = os.getenv('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
